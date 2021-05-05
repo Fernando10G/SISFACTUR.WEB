7aabc127490d670f3d92ed740bc528211d8ffcfa
Fernando10G <fer.core1096@gmail.com>
2021-05-05 12:32:35 -0500

    16

---
+++ SISTEM.FACTUR.BUSINESS/BULogin.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using SISTEM.FACTUR.CLIENTS;
+using SISTEM.FACTUR.ENTITY.Parametros;
+using SISTEM.FACTUR.ENTITY.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SISTEM.FACTUR.BUSINESS
+{
+    public class BULogin
+    {
+        private Client clients;
+        public BULogin()
+        {
+            clients = new Client();
+        }
+
+        public ResponseLogin Acceder(ENLogin paramss)
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<ResponseLogin>(clients.Post<ENLogin>("Login/Acceder", paramss));
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+    }
+}
---
+++ SISTEM.FACTUR/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using SISTEM.FACTUR.BUSINESS;
+using SISTEM.FACTUR.ENTITY.Encrypt;
+using SISTEM.FACTUR.ENTITY.Parametros;
+using SISTEM.FACTUR.ENTITY.Response;
+using SISTEM.FACTUR.WEB.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SISTEM.FACTUR.Controllers
+{
+    public class LoginController : Controller
+    {
+        private BULogin bulogin;
+
+        public LoginController()
+        {
+
+            bulogin = new BULogin();
+        }
+        [HttpPost]
+        public ActionResult Acceder(ENLogin paramss)
+        {
+            var clave = Encrypt.GetSHA256(paramss.pass);
+            paramss.pass = clave;
+            var rpt = bulogin.Acceder(paramss);
+            Session.Set(GlobalKey.CurrentUser, rpt);
+            SetCurrentUser(rpt);
+            return Json(new { dt = rpt });
+        }
+        protected void SetCurrentUser(ResponseLogin login)
+        {
+            Session["Username"] = login;
+        }
+    }
+}
